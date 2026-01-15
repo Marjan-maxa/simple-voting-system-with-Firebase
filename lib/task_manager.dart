746d7984362feb696a76344cb660179ada3c2cc4
Marjan-maxa <boktimd95@gmail.com>
2026-01-16 01:28:57 +0600

    added

---
+++ lib/task_manager.dart
@@ -0,0 +1,147 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TaskManager extends StatelessWidget {
+   TaskManager({super.key});
+
+   TextEditingController titleController=TextEditingController();
+   TextEditingController descriptionController=TextEditingController();
+
+   CollectionReference tasks=FirebaseFirestore.instance.collection('tasks');
+
+   Future<void> updateTask(String id,bool completed) async {
+
+     await tasks.doc(id).update(
+       {
+         'title':titleController.text,
+         'description':descriptionController.text,
+         'completed':completed,
+       }
+     );
+   }
+   Future<void> updateTaskStatus(String id,bool completed) async {
+
+     await tasks.doc(id).update(
+       {
+         'completed':completed,
+       }
+     );
+   }
+
+   Future<void> deleteTask(String id)async {
+     await tasks.doc(id).delete();
+   }
+
+   Future<void> addTaskFirebase()async {
+    await tasks.add(
+       {
+         'title':titleController.text,
+         'description':descriptionController.text,
+         'completed':false,
+
+       }
+     );
+   }
+
+  
+  
+  void showDialogbox(BuildContext context, [DocumentSnapshot? doc]){
+     if(doc!=null){
+       titleController.text=doc['title'];
+       descriptionController.text=doc['description'];
+     }else{
+       titleController.clear();
+       descriptionController.clear();
+     }
+    showDialog(context: context, builder: (_)=>AlertDialog(
+      title: Text(doc!=null ?'Update task':'Add task'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            controller: titleController,
+            decoration: InputDecoration(
+              labelText: 'Title'
+            ),
+          ),
+          TextFormField(
+            controller: descriptionController,
+            decoration: InputDecoration(
+              labelText: 'Description'
+            ),
+          )
+        ],
+      ),
+      actions: [
+        TextButton(onPressed: (){
+          Navigator.pop(context);
+        }, child: Text('cancel')),
+        ElevatedButton(onPressed: (){
+          doc!=null?updateTask(doc.id, doc['completed']):
+          addTaskFirebase();
+          Navigator.pop(context);
+        }, child: Text('Add'))
+      ],
+    ));
+  }
+
+  // 32 minutes after start .........................
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true ,
+        backgroundColor: Colors.blue,
+        title: Text('Task Manager',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+      ),
+
+      body: StreamBuilder<QuerySnapshot>(
+        stream: tasks.snapshots(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData) return Center(child: CircularProgressIndicator());
+          final task=snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: task.length,
+              itemBuilder: (context,index){
+              final doc=task[index];
+            return Slidable(
+              key: ValueKey(doc.id),
+              endActionPane: ActionPane(motion: DrawerMotion(), children: [
+                SlidableAction(onPressed: (_){
+                  deleteTask(doc.id);
+                },
+                backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  icon: Icons.delete,
+                  label: 'Delete',
+                )
+              ]),
+              child: Card(
+                child: ListTile(
+                  title: Text(doc['title']),
+                  subtitle: Text(doc['description']),
+                  leading: Checkbox(value: doc['completed'], onChanged: (val){
+                    updateTaskStatus(doc.id,val!);
+                  }),
+                  trailing: IconButton(onPressed: (){
+                    showDialogbox(context,doc);
+                  }, icon: Icon(Icons.edit)),
+                ),
+              ),
+            );
+          }
+          );
+        }
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        onPressed: (){
+          showDialogbox(context);
+        },child: Icon(Icons.add),),
+    );
+  }
+}
--- lib/vote_screen.dart
+++ lib/vote_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:project_firebase/task_manager.dart';
 
 class VoteScreen extends StatelessWidget {
   const VoteScreen({super.key});
@@ -21,52 +22,67 @@ class VoteScreen extends StatelessWidget {
         centerTitle: true ,
       ),
 
-      body:  StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('Candidates').snapshots(),
-        builder: (context, snapshot) {
-          if(snapshot.connectionState==ConnectionState.waiting){
-            return Center(child: CircularProgressIndicator());
-          }
-          final candidate=snapshot.data!.docs;
-          return Center(
-            child: GridView.builder(
-              shrinkWrap: true,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      body:  Column(
+        children: [
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('Candidates').snapshots(),
+            builder: (context, snapshot) {
+              if(snapshot.connectionState==ConnectionState.waiting){
+                return Center(child: CircularProgressIndicator());
+              }
+              final candidate=snapshot.data!.docs;
+              return Center(
+                child: GridView.builder(
+                  shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
 
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 0.55,
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 0.55,
 
-                ),
-                itemCount: candidate.length,
-                itemBuilder: (context,index){
-                final doc =candidate[index];
-                final  data=doc.data() as Map<String,dynamic>;
-                  return Card(
-                    elevation: 5,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(child: Image.network(data['imageUrl'],fit: BoxFit.cover,
-                          width: double.infinity,)),
-                          Text(data['Name'],style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                          Text(data['Votes'].toString(),style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              foregroundColor: Colors.white
-                            ),
-                              onPressed: ()=>voteCandidates(doc.id), child: Text('Given Vote'))
-                        ],
-                      ),
                     ),
-                  );
-                }
+                    itemCount: candidate.length,
+                    itemBuilder: (context,index){
+                    final doc =candidate[index];
+                    final  data=doc.data() as Map<String,dynamic>;
+                      return Card(
+                        elevation: 5,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(child: Image.network(data['imageUrl'],fit: BoxFit.cover,
+                              width: double.infinity,)),
+                              Text(data['Name'],style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                              Text(data['Votes'].toString(),style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  foregroundColor: Colors.white
+                                 ),
+                                  onPressed: ()=>voteCandidates(doc.id), child: Text('Given Vote')
+                              ),
+
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                ),
+              );
+            }
+          ),
+          const SizedBox(height: 100,),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white
             ),
-          );
-        }
+              onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>TaskManager()));
+              }, child: Text('Next'))
+        ],
       ),
     );
   }
